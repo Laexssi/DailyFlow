4c3ebb36cf368bec34e2a653b08c3033e3e8ba96
Laexssi <ktoornlaaldlaiepxssi@gmail.com>
2021-04-25 14:31:08 +0300

    add calendar event styles

--- src/screens/Analytics/index.vue
+++ src/screens/Analytics/index.vue
@@ -4,13 +4,6 @@
       <v-toolbar
       flat>
         <v-btn
-        outlined
-        class="mr-4"
-        color="grey darken-2"
-        @click="setToday">
-          Today
-        </v-btn>
-        <v-btn
         fab
         text
         small
@@ -20,6 +13,11 @@
             mdi-chevron-left
           </v-icon>
         </v-btn>
+
+        <v-toolbar-title v-if="$refs.calendar">
+          {{ $refs.calendar.title }}
+        </v-toolbar-title>
+
         <v-btn
         fab
         text
@@ -30,10 +28,9 @@
             mdi-chevron-right
           </v-icon>
         </v-btn>
-        <v-toolbar-title v-if="$refs.calendar">
-          {{ $refs.calendar.title }}
-        </v-toolbar-title>
+
         <v-spacer/>
+
         <v-menu
         bottom
         right>
@@ -42,28 +39,41 @@
             outlined
             color="grey darken-2"
             v-bind="attrs"
-            v-on="on">
+            v-on="on"
+            class="mr-1">
               <span>{{ typeToLabel[type] }}</span>
+
               <v-icon right>
                 mdi-menu-down
               </v-icon>
             </v-btn>
           </template>
+
           <v-list>
             <v-list-item @click="type = 'day'">
               <v-list-item-title>Day</v-list-item-title>
             </v-list-item>
+
             <v-list-item @click="type = 'week'">
               <v-list-item-title>Week</v-list-item-title>
             </v-list-item>
+
             <v-list-item @click="type = 'month'">
               <v-list-item-title>Month</v-list-item-title>
             </v-list-item>
+
             <v-list-item @click="type = '4day'">
               <v-list-item-title>4 days</v-list-item-title>
             </v-list-item>
           </v-list>
         </v-menu>
+
+        <v-btn
+        outlined
+        color="grey darken-2"
+        @click="setToday">
+          Today
+        </v-btn>
       </v-toolbar>
     </v-sheet>
 
@@ -193,9 +203,14 @@
     overflow-x: hidden;
   }
 
-  .v-event {
-    height: 100px !important;
-    width: auto;
+  .v-calendar-daily {
+    overflow: auto !important;
+    .v-event {
+      height: 65px !important;
+      width: auto !important;
+      border: 1px solid;
+      margin-bottom: 8px !important;
+    }
   }
 
   .v-calendar-daily__body {
