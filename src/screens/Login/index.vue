82672d44b94a2e3b12e14363f7ff795623d14f2d
Laexssi <ktoornlaaldlaiepxssi@gmail.com>
2021-04-25 19:57:58 +0300

    add login info

--- src/screens/Login/index.vue
+++ src/screens/Login/index.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="main-screen--content form-container">
+    <h1 class="login-header">
+      Daily Flow
+    </h1>
+
+    <div class="login__info">
+      <p class="login__info-text">
+        Создавайте карточки занятий, которые сохранятся в вашу библиотеку.
+      </p>
+
+      <p class="login__info-text">
+        Добавляйте лейблы вашим занятием.
+      </p>
+
+      <p class="login__info-text">
+        Составляйте переиспользуемые планы с настройками продолжительности.
+      </p>
+
+      <p class="login__info-text">
+        Отмечайте выполнения занятий и следите за своей статистикой.
+      </p>
+    </div>
+
     <p>
       Login with Google
     </p>
@@ -46,5 +68,23 @@
     justify-content: center;
     align-items: center;
     flex-grow: 1;
+  }
+
+  .login-header {
+    font-size: 20px;
+    text-align: center;
+  }
+
+  .login__info {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 8px;
+    @include between-children() {
+      margin-bottom: 8px;
+    }
+  }
+
+  .login__info-text {
   }
 </style>
